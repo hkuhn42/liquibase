ba385cfac556da818aba09134d7ed50f0372f8d0
Harald Kuhn <harald.kuhn@turnfriendly.com>
2026-04-24 08:15:35 +0200

    Defaults for splitOuztput and ommitSchemas are now true

--- liquibase-standard/src/main/java/liquibase/command/core/UpdateSqlCommandStep.java
+++ liquibase-standard/src/main/java/liquibase/command/core/UpdateSqlCommandStep.java
@@ -1,54 +1,59 @@
 package liquibase.command.core;
 
-import liquibase.UpdateSummaryEnum;
-import liquibase.command.*;
-import liquibase.command.core.helpers.DatabaseChangelogCommandStep;
-import liquibase.database.Database;
-import liquibase.util.LoggingExecutorTextUtil;
-
 import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 
+import liquibase.UpdateSummaryEnum;
+import liquibase.command.CommandArgumentDefinition;
+import liquibase.command.CommandBuilder;
+import liquibase.command.CommandDefinition;
+import liquibase.command.CommandResultsBuilder;
+import liquibase.command.CommandScope;
+import liquibase.command.CommonArgumentNames;
+import liquibase.command.core.helpers.DatabaseChangelogCommandStep;
+import liquibase.database.Database;
+import liquibase.util.LoggingExecutorTextUtil;
+
 public class UpdateSqlCommandStep extends AbstractUpdateCommandStep {
-
-    public static final String[] COMMAND_NAME = {"updateSql"};
-    public static final String[] LEGACY_COMMAND_NAME = {"migrateSql"};
-
-    public static final CommandArgumentDefinition<String> CHANGELOG_FILE_ARG;
-    public static final CommandArgumentDefinition<String> LABEL_FILTER_ARG;
-    public static final CommandArgumentDefinition<String> CONTEXTS_ARG;
-    public static final CommandArgumentDefinition<Writer> OUTPUT_WRITER;
+    
+    public static final String[]                           COMMAND_NAME        = { "updateSql" };
+    public static final String[]                           LEGACY_COMMAND_NAME = { "migrateSql" };
+    
+    public static final CommandArgumentDefinition<String>  CHANGELOG_FILE_ARG;
+    public static final CommandArgumentDefinition<String>  LABEL_FILTER_ARG;
+    public static final CommandArgumentDefinition<String>  CONTEXTS_ARG;
+    public static final CommandArgumentDefinition<Writer>  OUTPUT_WRITER;
     public static final CommandArgumentDefinition<Boolean> OUTPUT_DEFAULT_SCHEMA_ARG;
     public static final CommandArgumentDefinition<Boolean> OUTPUT_DEFAULT_CATALOG_ARG;
     public static final CommandArgumentDefinition<Boolean> SPLIT_SQL_OUTPUT_ARG;
-
+    
     static {
         CommandBuilder builder = new CommandBuilder(COMMAND_NAME, LEGACY_COMMAND_NAME);
         CHANGELOG_FILE_ARG = builder.argument(CommonArgumentNames.CHANGELOG_FILE, String.class).required()
-                .description("The root changelog").build();
+            .description("The root changelog").build();
         LABEL_FILTER_ARG = builder.argument("labelFilter", String.class)
-                .addAlias("labels")
-                .description("Changeset labels to match").build();
+            .addAlias("labels")
+            .description("Changeset labels to match").build();
         CONTEXTS_ARG = builder.argument("contextFilter", String.class)
-                .addAlias("contexts")
-                .description("Changeset contexts to match").build();
+            .addAlias("contexts")
+            .description("Changeset contexts to match").build();
         OUTPUT_WRITER = builder.argument("outputWriter", Writer.class)
-                .hidden().build();
+            .hidden().build();
         OUTPUT_DEFAULT_SCHEMA_ARG = builder.argument("outputDefaultSchema", Boolean.class)
-                .description("Control whether names of objects in the default schema are fully qualified or not. If true they are. If false, only objects outside the default schema are fully qualified")
-                .defaultValue(true)
-                .build();
+            .description("Control whether names of objects in the default schema are fully qualified or not. If true they are. If false, only objects outside the default schema are fully qualified")
+            .defaultValue(true)
+            .build();
         OUTPUT_DEFAULT_CATALOG_ARG = builder.argument("outputDefaultCatalog", Boolean.class)
-                .description("Control whether names of objects in the default catalog are fully qualified or not. If true they are. If false, only objects outside the default catalog are fully qualified")
-                .defaultValue(true)
-                .build();
+            .description("Control whether names of objects in the default catalog are fully qualified or not. If true they are. If false, only objects outside the default catalog are fully qualified")
+            .defaultValue(true)
+            .build();
         SPLIT_SQL_OUTPUT_ARG = builder.argument("splitOutput", Boolean.class)
-                .description("Write output SQL to a separate file per changeset when outputFile is set")
-                .defaultValue(false)
-                .build();
+            .description("Write output SQL to a separate file per changeset when outputFile is set")
+            .defaultValue(true)
+            .build();
     }
-
+    
     @Override
     public List<Class<?>> requiredDependencies() {
         List<Class<?>> dependencies = new ArrayList<>();
@@ -56,7 +61,7 @@ public class UpdateSqlCommandStep extends AbstractUpdateCommandStep {
         dependencies.addAll(super.requiredDependencies());
         return dependencies;
     }
-
+    
     @Override
     public void run(CommandResultsBuilder resultsBuilder) throws Exception {
         final CommandScope commandScope = resultsBuilder.getCommandScope();
@@ -68,15 +73,15 @@ public class UpdateSqlCommandStep extends AbstractUpdateCommandStep {
         }
         super.run(resultsBuilder);
     }
-
+    
     @Override
     public String[][] defineCommandNames() {
-        return new String[][]{
+        return new String[][] {
                 COMMAND_NAME,
                 LEGACY_COMMAND_NAME
         };
     }
-
+    
     @Override
     public void adjustCommandDefinition(CommandDefinition commandDefinition) {
         commandDefinition.setShortDescription("Generate the SQL to deploy changes in the changelog which have not been deployed");
@@ -84,28 +89,27 @@ public class UpdateSqlCommandStep extends AbstractUpdateCommandStep {
             commandDefinition.setHidden(true);
         }
     }
-
-
+    
     @Override
     public String getChangelogFileArg(CommandScope commandScope) {
         return commandScope.getArgumentValue(CHANGELOG_FILE_ARG);
     }
-
+    
     @Override
     public String getContextsArg(CommandScope commandScope) {
         return commandScope.getArgumentValue(CONTEXTS_ARG);
     }
-
+    
     @Override
     public String getLabelFilterArg(CommandScope commandScope) {
         return commandScope.getArgumentValue(LABEL_FILTER_ARG);
     }
-
+    
     @Override
     public String[] getCommandName() {
         return COMMAND_NAME;
     }
-
+    
     @Override
     public UpdateSummaryEnum getShowSummary(CommandScope commandScope) {
         return UpdateSummaryEnum.OFF;
